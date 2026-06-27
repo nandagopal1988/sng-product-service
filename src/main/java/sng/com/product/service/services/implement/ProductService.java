5a330581b6ba8c144d8e089dd33c7312abf845f9
nandagopal1988 <152818907+nandagopal1988@users.noreply.github.com>
2026-06-27 12:04:44 +0530

    Revert "test commit for review"

--- src/main/java/sng/com/product/service/services/implement/ProductService.java
+++ src/main/java/sng/com/product/service/services/implement/ProductService.java
@@ -17,7 +17,7 @@ public class ProductService implements IProductService {
     {
         Product objProduct = new Product();
         objProduct.ProductId =1;
-        objProduct.ProductCode="1245";
+        objProduct.ProductCode="00245";
         objProduct.ProductName="Product_Name";
         objProduct.ProductDescription="Product_Description";
         objProduct.ProductDeliveryDate= null;
