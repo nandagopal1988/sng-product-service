769e00229b436a1f6b87830bae7a21340dcfcaa6
nandagopal1988 <152818907+nandagopal1988@users.noreply.github.com>
2026-06-27 12:28:12 +0530

    Update ProductService.java

--- src/main/java/sng/com/product/service/services/implement/ProductService.java
+++ src/main/java/sng/com/product/service/services/implement/ProductService.java
@@ -18,7 +18,7 @@ public class ProductService implements IProductService {
         Product objProduct = new Product();
         objProduct.ProductId =1;
         objProduct.ProductCode="1245";
-        objProduct.ProductName="Product_Name";
+        objProduct.ProductName="Product001";
         objProduct.ProductDescription="Product_Description";
         objProduct.ProductDeliveryDate= null;
         objProductlst.add(objProduct);
